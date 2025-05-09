feb308555ad6ea1bf02cb6c3d9275ff3106460a7
Yelyzaveta <liza8203788@gmail.com>
2025-05-09 15:38:07 +0300

    Написала всі функції класу PuzzleField.cpp окрім setBlackCellsManual()

--- PuzzleField.cpp
+++ PuzzleField.cpp
@@ -27,7 +27,7 @@ void PuzzleField::reserField(){
         }
     }
 }
-void fillFieldManual(){
+void PuzzleField::fillFieldManual(){
     cout << "\nEnter cell values (-1 for blank), for each row:\n";
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
@@ -36,7 +36,7 @@ void fillFieldManual(){
     cout << "\nField:\n";
     print();
 }
-void fillFieldAuto(){
+void PuzzleField::fillFieldAuto(){
     srand(time(0));
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
@@ -50,10 +50,128 @@ void fillFieldAuto(){
     cout << "\nField";
     print();
 }
-void setBlackCellsManual(){}
-void solve(int x=0, int y=0){}
-void print() const{}
-bool areWhiteCellsConnected(){}
-bool inBounds(int x, int y) const{}
-bool isNumberUnique(){}
-void outputStep(int x, int y, bool was_assigned_to_black){}
+void PuzzleField::setBlackCellsManual(){}
+void PuzzleField::solve(int x, int y){
+    if(x==rows){
+        return isNumberUnique();
+    }
+
+    int next_x = x, next_y = y+1;
+    if(next_y == cols){
+        next_y = 0;
+        next_x++;
+    }
+
+    if(field[x][y].number == -1){
+        return solve(next_x, next_y);
+    }
+
+    bool can_be_assigned_to_black = true;
+    int move_row[4] = {-1, 1, 0, 0};
+    int move_col[4] = {0, 0, -1, 1};
+
+    for(int i=0; i<4; i++){
+        int new_row = x + move_row[i];
+        int new_col = y + move_col[i];
+        if(inBounds(new_row, new_col) && field[new_row][new_col].state == BLACK){
+            can_be_assigned_to_black = false;
+        }
+    }
+
+    if(can_be_assigned_to_black){
+        field[x][y].state = BLACK;
+        outputStep(x, y, true);
+        if(areWhiteCellsConnected() && solve(net_x, next_y)){
+            return true;
+        }
+        field[x][y].state = WHITE;
+    }
+
+    if(solve(next_x, next_y)){
+        return true;
+    }
+    return false;
+}
+void PuzzleField::print() const{
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            if(field[i][j].state = BLACK){
+                cout << "X ";
+            }
+            else if(field.[i][j].number != -1){
+                cout << field[i][j].number << " ";
+            }
+            else{
+                cout << ". ";
+            }
+        }
+        cout << "\n";
+    }
+}
+bool PuzzleField::areWhiteCellsConnected(){
+    int move_roe[4] = {-1, 1, 0, 0};
+    int move_col[4] = {0, 0, -1, 1};
+
+    for(int x=0; x<rows; x++){
+        for(int y=0; y<cols; y++){
+            if(field[x][y].state = BLACK){
+                continue;
+            }
+
+            boll all_neighbors_black = true;
+
+            for(int d=0; d<4; d++){
+                int next_x = x + move_row[d];
+                it next_y = y + move_col[d];
+
+                if(inBounds(next_x, next_y) && field[next_x][next_y].state != BLACK){
+                    all_neighbors_black = false;
+                    break;
+                }
+            }
+
+            if(all_neighbors_black){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+bool PuzzleField::inBounds(int x, int y) const{
+    return x >= 0 && x < rows && y >= 0 && y < cols;
+}
+bool PuzzleField::isNumberUnique(){
+    int numbers[6];
+    int index = -1;
+    for(int region_id = 1; region_id <= MAX_REDION_ID; region_id++){
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(field[i][j].state != BLACK && field[i][j].region == region_id){
+                    int num = field[i][j].number;
+                    index += 1;
+                    if(num!= -1){
+                        numbers[index] = num;
+                    }
+                }
+            }
+        }
+        for(index = 0; index < 6; index++){
+            for(int i = index+1; i<6; i++){
+                if(numbers[index] == numbers[i]){
+                    return false;
+                }
+            }
+        }
+        index = -1;
+    }
+    return true;
+}
+void PuzzleField::outputStep(int x, int y, bool was_assigned_to_black){
+    if(was_assigned_to_black){
+        cout << "Cell (" << x << ", " << y << ") was assigned to black.\n";
+    }
+    else{
+        cout << "Cell (" << x << ", " << y << ") was assigned to white.\n";
+    }
+    print();
+}
